50c704d8f80bad770980788ba8c65bffc524d594
MaxTheEternal <98349129+MaxTheEternal@users.noreply.github.com>
2024-12-11 21:38:21 +0100

    Parking Node kompiliert

--- src/main.cpp
+++ src/main.cpp
@@ -23,37 +23,8 @@ const int clockInhibit = A5;
  
 const int numBits = 40;   /* Set to 8 * number of shift registers */
 
+void displayNumber(int number);
 
-void displayNumber (int number){
-  uint16_t firstDigitEncoding[] = 
-  {0b1000111100011111,//0
-  0b1110111111011111,//1
-  0b0100111100111111,//2
-  0b0100111110011111,//3
-  0b0010111111011111//4
-  };
-  uint16_t lastDigitEncoding[] =  
-  {0b1111100011101001,//0
-  0b1111111011111101,//1
-  0b1111010011101011,//2
-  0b1111010011111001,//3
-  0b1111001011111101,//4
-  0b1111000111111001,//5
-  0b1111000111101001,//6
-  0b1111110011111101,//7
-  0b1111000011101001,//8
-  0b1111000011111001//9
-  };
-  byte lastDigit = number%10;
-  byte firstDigit = (number/10)%10;
-
-  int data = firstDigitEncoding[firstDigit] & lastDigitEncoding[lastDigit];
-
-  digitalWrite(DISPLAY_RCLK, LOW);
-  shiftOut(DISPLAY_SER, DISPLAY_SRCLK, LSBFIRST, (data >> 8) & 0xff);
-  shiftOut(DISPLAY_SER, DISPLAY_SRCLK, LSBFIRST, data  & 0xff);
-  digitalWrite(DISPLAY_RCLK, HIGH);
-}
 
 // byte toByte(int value) {
 //     return (value >= 0 && value <= 255) ? static_cast<byte>(value) : 0;
@@ -75,7 +46,8 @@ void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(clockInhibit, OUTPUT);
 
-  
+  ParkingNode* entryNode = ParkingNode::init();
+  Serial.print(entryNode->childCount);
 
 }
  
@@ -122,3 +94,34 @@ void loop() {
 
 }
 
+
+void displayNumber (int number){
+  uint16_t firstDigitEncoding[] = 
+  {0b1000111100011111,//0
+  0b1110111111011111,//1
+  0b0100111100111111,//2
+  0b0100111110011111,//3
+  0b0010111111011111//4
+  };
+  uint16_t lastDigitEncoding[] =  
+  {0b1111100011101001,//0
+  0b1111111011111101,//1
+  0b1111010011101011,//2
+  0b1111010011111001,//3
+  0b1111001011111101,//4
+  0b1111000111111001,//5
+  0b1111000111101001,//6
+  0b1111110011111101,//7
+  0b1111000011101001,//8
+  0b1111000011111001//9
+  };
+  byte lastDigit = number%10;
+  byte firstDigit = (number/10)%10;
+
+  int data = firstDigitEncoding[firstDigit] & lastDigitEncoding[lastDigit];
+
+  digitalWrite(DISPLAY_RCLK, LOW);
+  shiftOut(DISPLAY_SER, DISPLAY_SRCLK, LSBFIRST, (data >> 8) & 0xff);
+  shiftOut(DISPLAY_SER, DISPLAY_SRCLK, LSBFIRST, data  & 0xff);
+  digitalWrite(DISPLAY_RCLK, HIGH);
+}
